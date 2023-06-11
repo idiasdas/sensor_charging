1458942d13042c5d9e9fcfb86dea6c8936823f16
idiasdas <igordias.ti@gmail.com>
2023-06-11 10:11:43 +0200

    More documentation.
    	modified:   scheduling_TSP.py

--- scheduling_TSP.py
+++ scheduling_TSP.py
@@ -2,6 +2,16 @@ from basic_functions import *
 import itertools
 # checks all possible sequences of tasks for each drone, returns the maximum time of the sequences with minimum time
 def scheduling_TSP(tasks, n_drones, drone_speed = 10.2):
+    """Solves the scheduling problem by checking all possible sequences of tasks for each drone, returns the maximum time of the sequences with minimum time
+
+    Args:
+        tasks (list): List of tasks to be scheduled
+        n_drones (int): Number of drones available
+        drone_speed (float, optional): Drone's speed. Defaults to 10.2.
+
+    Returns:
+        list: [ List of scheduled tasks, finish time of the last drone]
+    """
     total_time = []
     best_sequences = []
     for drone in range(n_drones):
