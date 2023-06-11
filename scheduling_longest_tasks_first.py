f4459bcd4e79a6193c663e1e9d909b84013dc719
idiasdas <igordias.ti@gmail.com>
2023-06-11 13:32:41 +0200

    Created optimized version of ToF scheduling 	modified:   scheduling_longest_tasks_first.py

--- scheduling_longest_tasks_first.py
+++ scheduling_longest_tasks_first.py
@@ -69,3 +69,65 @@ def scheduling_algo_longest_tasks_first(tasks,n_drones,drone_speed = 10.2):
                 current_tasks.remove(task)
                 n_tasks = n_tasks - 1
     return [done,time]
+
+def scheduling_longest_tasks_first_optimized(tasks, n_drones, drone_speed = 10.2):
+    """Scheduling algorithm, longest tasks first
+
+    Args:
+        tasks (list): List of tasks to be scheduled
+        n_drones (int): Number of drones available
+        drone_speed (float, optional): Drone's speed. Defaults to 10.2.
+
+    Returns:
+        list: List of scheduled tasks, with start and finish times.
+    """
+    time = 0
+    n_tasks = len(tasks)
+    base_station = (0,0,0)
+    current_tasks = []
+    last_position = []
+    status_free = []
+    for drone in range(n_drones):
+        last_position += [base_station]
+        status_free += [True]
+    done = []
+    
+    while n_tasks > 0:
+        go = True
+        while(go):
+            # order tasks by duration from longest to shortest
+            for task in tasks:
+                task["ToF"] = dist(last_position[task["drone"]],task["position"])/drone_speed
+                if(get_wait_time(task, current_tasks,time) > task["ToF"]):
+                    task["total_wait"] = max(get_wait_time(task, current_tasks,time),task["ToF"])
+                else:
+                    task["total_wait"] = task["ToF"]
+            tasks.sort(key = lambda x: x["time"],reverse=True)
+
+            # assign tasks
+            task_added = False
+            for task in list(tasks):
+                if(status_free[task["drone"]] and len( [x for x in current_tasks if x["drone"] == task["drone"]]) == 0):
+                    task["start"] = time + task["total_wait"]
+                    task["end"] = task["start"] + task["time"]
+                    last_position[task["drone"]] = task["position"]
+                    current_tasks += [task]
+                    tasks.remove(task)
+                    task_added = True
+                    status_free[task["drone"]] = False
+                    break
+                status_free[task["drone"]] = False
+                    
+            if(not task_added):
+                go = False
+        
+        # Forward in time
+        time = min([x["end"] for x in current_tasks])
+        #Finish tasks
+        for task in list(current_tasks):
+            if(time >= task["end"]):
+                done += [task]
+                status_free[task["drone"]] = True
+                current_tasks.remove(task)
+                n_tasks = n_tasks - 1
+    return [done,time]
