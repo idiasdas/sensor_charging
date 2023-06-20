9e80657385fe1353b367086cf0a2090a35dbb6e5
idiasdas <igordias.ti@gmail.com>
2023-06-20 15:09:35 +0200

    Added some documentation

--- test_algo_comparison.py
+++ test_algo_comparison.py
@@ -68,53 +68,64 @@ def algo_comparison(algo1, algo2, p = 5, drone_speed = 0.5, i_max = 50, drones =
     
     return is_equivalent
 
-def test_equivalence(algos, file_name,plot_recharge_time = True, plot_first_example=False, drone_based = True):
-    print("* Testing "+algos[0]["algo"].__name__+" and " + algos[1]["algo"].__name__ + " for equivalence.")
-    if algo_comparison(algos[0]["algo"], algos[1]["algo"],drone_based=drone_based, plot_first_example=plot_first_example):
+def test_equivalence(algo1, algo2, file_name,plot_recharge_time = True, plot_first_example=False, drone_based = True):
+    """Compares two algorithms, if they are not equivalent plots a figure with the total recharge time for each algorithm.
+
+    Args:
+        algo1 (dict): Scheduling function 1.
+        algo2 (dict): Scheduling function 2.
+        file_name (str): path + name for output figure if needed.
+        plot_recharge_time (bool, optional): If True plots the total recharge time if agorithms are different. Defaults to True.
+        plot_first_example (bool, optional): If True plots the schedule of the first example where the algorithms are different. Defaults to False.
+        drone_based (bool, optional): If True, runs the algorithms on the DBLP output, else uses SBLP as input. Defaults to True.
+    """    
+    print("* Testing "+algo1["algo"].__name__+" and " + algo2["algo"].__name__ + " for equivalence.")
+    if algo_comparison(algo1["algo"], algo2["algo"],drone_based=drone_based, plot_first_example=plot_first_example):
         print("* (SUCCESS)")
     else:
         if(plot_recharge_time):
             print("* Plotting difference between algorithms over all inputs")
             if not drone_based:
-                plot_total_recharge_time_SMILP_DATA(algos, file_name=file_name[:-4] + "(SBLP).eps")
-                plot_total_recharge_time(algos, file_name=file_name[:-4] + "(DBLP).eps", legend_outside=False)
+                plot_total_recharge_time_SMILP_DATA([algo1,algo2], file_name=file_name[:-4] + "(SBLP).eps")
+                plot_total_recharge_time([algo1,algo2], file_name=file_name[:-4] + "(DBLP).eps", legend_outside=False)
             else:
-                plot_total_recharge_time(algos, file_name=file_name, legend_outside=False)
+                plot_total_recharge_time([algo1,algo2], file_name=file_name, legend_outside=False)
         print("* (FAIL)")
     print("---------------------------------------------------------------")
 
 def test_all():
+    """Compares the optmized/revised versions with the algorithms old versions."""
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_DB_WT, "label":"DB-WT","line":"b-"}
     algo2 = {"algo":scheduling_DB_WT_optimized, "label":"DB-WT optimized","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_DB_WT.eps")
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_DB_WT.eps")
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_DB_TOF, "label":"DB-TOF","line":"b-"}
     algo2 = {"algo":scheduling_DB_TOF_optimized, "label":"DB-TOF optimized","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_DB_TOF.eps")
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_DB_TOF.eps")
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_DB_LTF, "label":"DB-LTF","line":"b-"}
     algo2 = {"algo":scheduling_DB_LTF_optimized,"label":"DB-LTF optimized","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_DB_LTF.eps")
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_DB_LTF.eps")
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_DB_STF, "label":"DB-STF","line":"b-"}
     algo2 = {"algo":scheduling_DB_STF_optimized, "label":"DB-STF optimized","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_DB_STF.eps")
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_DB_STF.eps")
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_SB_WT, "label":"SB-WT","line":"b-"}
     algo2 = {"algo":scheduling_SB_WT_revised, "label":"SB-WT revised","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_SB_WT.eps", drone_based=False)
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_SB_WT.eps", drone_based=False)
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_SB_LTF, "label":"SB-LTF","line":"b-"}
     algo2 = {"algo":scheduling_SB_LTF_revised, "label":"SB-LTF revised","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_SB_LTF.eps", drone_based=False)
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_SB_LTF.eps", drone_based=False)
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_SB_STF, "label":"SB-STF","line":"b-"}
     algo2 = {"algo":scheduling_SB_STF_revised, "label":"SB-STF revised","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_SB_STF.eps", drone_based=False)
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_SB_STF.eps", drone_based=False)
     # -----------------------------------------------------------------------------------------------------
     algo1 = {"algo":scheduling_SB_TOF, "label":"SB-TOF","line":"b-"}
     algo2 = {"algo":scheduling_SB_TOF_revised, "label":"SB-TOF revised","line":"r-"}
-    test_equivalence([algo1, algo2], "tests_outputs/test_eq_SB_TOF.eps", drone_based=False)
+    test_equivalence(algo1, algo2, "tests_outputs/test_eq_SB_TOF.eps", drone_based=False)
 
 test_all()
