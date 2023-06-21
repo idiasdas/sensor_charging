1af441e3b5f03f1dc33a9d29ecb8648acb2abf01
idiasdas <igordias.ti@gmail.com>
2023-06-21 15:48:29 +0200

    Formatation update

--- generate_figures_GLOBECOM.py
+++ generate_figures_GLOBECOM.py
@@ -21,31 +21,31 @@ def generate_all_figures(file_path = "figures/"):
         file_path (str, optional): Path to save the figures. Defaults to "figures/".
     """    
     db_old_algos = [
-        {"algo":scheduling_DB_TOF,              "label":"DB-TOF",       "line":"y-"},
-        {"algo":scheduling_DB_WT,               "label":"DB-WT",        "line":"g-"},
-        {"algo":scheduling_DB_STF,              "label":"DB-STF",       "line":"k-"},
-        {"algo":scheduling_DB_LTF,              "label":"DB-LTF",       "line":"b-"},
-        {"algo":scheduling_TSP,                 "label":"TSP",          "line":"r-"}]
+        {"algo": scheduling_DB_TOF,              "label": "DB-TOF",     "line": "y-"},
+        {"algo": scheduling_DB_WT,               "label": "DB-WT",      "line": "g-"},
+        {"algo": scheduling_DB_STF,              "label": "DB-STF",     "line": "k-"},
+        {"algo": scheduling_DB_LTF,              "label": "DB-LTF",     "line": "b-"},
+        {"algo": scheduling_TSP,                 "label": "TSP",        "line": "r-"}]
     
     db_optimimzed_algos = [
-        {"algo":scheduling_DB_TOF_optimized,    "label":"DB-TOF",       "line":"y-"},
-        {"algo":scheduling_DB_WT_optimized,     "label":"DB-WT",        "line":"g-"},
-        {"algo":scheduling_DB_STF_optimized,    "label":"DB-STF",       "line":"k-"},
-        {"algo":scheduling_DB_LTF_optimized,    "label":"DB-LTF",       "line":"b-"},
-        {"algo":scheduling_TSP,                 "label":"TSP",          "line":"r-"}]
+        {"algo": scheduling_DB_TOF_optimized,    "label": "DB-TOF",     "line": "y-"},
+        {"algo": scheduling_DB_WT_optimized,     "label": "DB-WT",      "line": "g-"},
+        {"algo": scheduling_DB_STF_optimized,    "label": "DB-STF",     "line": "k-"},
+        {"algo": scheduling_DB_LTF_optimized,    "label": "DB-LTF",     "line": "b-"},
+        {"algo": scheduling_TSP,                 "label": "TSP",        "line": "r-"}]
     
     sb_revised_algos_plusTSP = [
-        {"algo":scheduling_SB_TOF_revised,      "label":"SB-TOF",       "line":"y-"},
-        {"algo":scheduling_SB_WT_revised,       "label":"SB-WT",        "line":"g-"},
-        {"algo":scheduling_SB_STF_revised,      "label":"SB-STF",       "line":"k-"},
-        {"algo":scheduling_SB_LTF_revised,      "label":"SB-LTF",       "line":"b-"},
-        {"algo":scheduling_TSP,                 "label":"TSP",          "line":"r-"}]
+        {"algo": scheduling_SB_TOF_revised,      "label": "SB-TOF",     "line": "y-"},
+        {"algo": scheduling_SB_WT_revised,       "label": "SB-WT",      "line": "g-"},
+        {"algo": scheduling_SB_STF_revised,      "label": "SB-STF",     "line": "k-"},
+        {"algo": scheduling_SB_LTF_revised,      "label": "SB-LTF",     "line": "b-"},
+        {"algo": scheduling_TSP,                 "label": "TSP",        "line": "r-"}]
     
     sb_revised_algos = [
-        {"algo":scheduling_SB_TOF_revised,      "label":"SB-TOF",       "line":"y-"},
-        {"algo":scheduling_SB_WT_revised,       "label":"SB-WT",        "line":"g-"},
-        {"algo":scheduling_SB_STF_revised,      "label":"SB-STF",       "line":"k-"},
-        {"algo":scheduling_SB_LTF_revised,      "label":"SB-LTF",       "line":"b-"}]
+        {"algo": scheduling_SB_TOF_revised,      "label": "SB-TOF",     "line": "y-"},
+        {"algo": scheduling_SB_WT_revised,       "label": "SB-WT",      "line": "g-"},
+        {"algo": scheduling_SB_STF_revised,      "label": "SB-STF",     "line": "k-"},
+        {"algo": scheduling_SB_LTF_revised,      "label": "SB-LTF",     "line": "b-"}]
     
     # Make figure using DB-LP output for old DB algorithms plus optimal with output from GLOBECOM backup
     plot_recharge_time_with_optimal_revised(db_old_algos, file_name = file_path + "RT_DBLP_OLD-DB-algos_OPTIMAL.eps", fig_title="OLD DB Algorithms (870)")
@@ -68,24 +68,24 @@ def generate_all_figures(file_path = "figures/"):
 
 def RT_comparison(file_path = "figures/new_figures/"):
     db_optimimzed_algos = [
-        {"algo":scheduling_DB_TOF_optimized,    "label":"TOF",  "line":"y-"},
-        {"algo":scheduling_DB_WT_optimized,     "label":"WT",   "line":"g-"},
-        {"algo":scheduling_DB_STF_optimized,    "label":"STF",  "line":"k-"},
-        {"algo":scheduling_DB_LTF_optimized,    "label":"LTF",  "line":"b-"},
-        {"algo":scheduling_TSP,                 "label":"TSP",  "line":"r-"}]
+        {"algo": scheduling_DB_TOF_optimized,    "label": "TOF",        "line": "y-"},
+        {"algo": scheduling_DB_WT_optimized,     "label": "WT",         "line": "g-"},
+        {"algo": scheduling_DB_STF_optimized,    "label": "STF",        "line": "k-"},
+        {"algo": scheduling_DB_LTF_optimized,    "label": "LTF",        "line": "b-"},
+        {"algo": scheduling_TSP,                 "label": "TSP",        "line": "r-"}]
     
     sb_revised_algos_plusTSP = [
-        {"algo":scheduling_SB_TOF_revised,      "label":"TOF",  "line":"y-"},
-        {"algo":scheduling_SB_WT_revised,       "label":"WT",   "line":"g-"},
-        {"algo":scheduling_SB_STF_revised,      "label":"STF",  "line":"k-"},
-        {"algo":scheduling_SB_LTF_revised,      "label":"LTF",  "line":"b-"},
-        {"algo":scheduling_TSP,                 "label":"TSP",  "line":"r-"}]
+        {"algo": scheduling_SB_TOF_revised,      "label": "TOF",        "line": "y-"},
+        {"algo": scheduling_SB_WT_revised,       "label": "WT",         "line": "g-"},
+        {"algo": scheduling_SB_STF_revised,      "label": "STF",        "line": "k-"},
+        {"algo": scheduling_SB_LTF_revised,      "label": "LTF",        "line": "b-"},
+        {"algo": scheduling_TSP,                 "label": "TSP",        "line": "r-"}]
     
     sb_revised_algos = [
-        {"algo":scheduling_SB_TOF_revised,      "label":"TOF",  "line":"y-"},
-        {"algo":scheduling_SB_WT_revised,       "label":"WT",   "line":"g-"},
-        {"algo":scheduling_SB_STF_revised,      "label":"STF",  "line":"k-"},
-        {"algo":scheduling_SB_LTF_revised,      "label":"LTF",  "line":"b-"}]
+        {"algo": scheduling_SB_TOF_revised,      "label": "TOF",        "line": "y-"},
+        {"algo": scheduling_SB_WT_revised,       "label": "WT",         "line": "g-"},
+        {"algo": scheduling_SB_STF_revised,      "label": "STF",        "line": "k-"},
+        {"algo": scheduling_SB_LTF_revised,      "label": "LTF",        "line": "b-"}]
     
 
     plot_total_recharge_time(algos=db_optimimzed_algos, file_name = file_path + "DBLP_DB_ALGOS.eps", fig_title="DB-LP + DB Algorithms", legend_style=3)
